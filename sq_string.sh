7d97766e55d77c01afc17c5718236983f389f63b
Steven Fairchild <steve51516@gmail.com>
2020-10-29 08:35:23 -0400

    more changes

--- sq_string.sh
+++ sq_string.sh
@@ -1,16 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 vertMirror() {
 	for ((i=0; i < ${#origArr[@]}; i++)); do
                 sArr[$i]=$(echo ${origArr[$i]} | rev)
         done
-	final=$(echo ${sArr[*]} | tr ' ' '\r')
+	echo $(echo ${sArr[*]} | tr ',' '\r')
 }
 horMirror() {
         local count=$((${#origArr[@]} - 1))
         for ((i=$count,j=0; i >= 0; i--,j++)); do
         	sArr[$i]=${origArr[$j]}
         done
-	final=$(echo ${sArr[*]} | tr ' ' '\r')
+	echo $(echo ${sArr[*]} | tr ',' '\r')
 }
 oper() {
 	IFS=',' read -ra origArr <<< $2
@@ -19,8 +19,8 @@ oper() {
 	elif [ $1 == "hor_mirror" ]; then
 		local output=$(horMirror)
 	fi
-	#echo ${output[*]}
-	echo $output
+	for i in ${output[@]}; do
+		echo $i
+	done
 }
-array=$(oper $1 $2)
 exit 0
