cb489b300f83ef7ab0c7d74c0bff57f4f30d3777
Steven Fairchild <steve51516@gmail.com>
2020-10-29 10:44:59 -0400

    completed sq_string

--- sq_string.sh
+++ sq_string.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 vertMirror() {
 	for ((i=0; i < ${#origArr[@]}; i++)); do
-                sArr[$i]=$(echo ${origArr[$i]} | rev)
+                local sArr[$i]=$(echo ${origArr[$i]} | rev)
         done
-	echo $(echo ${sArr[*]} | tr ',' '\r')
+	echo $(echo ${sArr[*]} | tr ' ' '\r')
 }
 horMirror() {
         local count=$((${#origArr[@]} - 1))
         for ((i=$count,j=0; i >= 0; i--,j++)); do
-        	sArr[$i]=${origArr[$j]}
+        	local sArr[$i]=${origArr[$j]}
         done
-	echo $(echo ${sArr[*]} | tr ',' '\r')
+	echo $(echo ${sArr[*]} | tr ' ' '\r')
 }
 oper() {
 	IFS=',' read -ra origArr <<< $2
-	if [ $1 == "vert_mirror" ]; then
-		local output=$(vertMirror)
-	elif [ $1 == "hor_mirror" ]; then
-		local output=$(horMirror)
+	if [ $1 == "vertMirror," ]; then
+		final=$(vertMirror)
+	elif [ $1 == "horMirror," ]; then
+		final=$(horMirror)
+	else
+		echo "error"
 	fi
-	for i in ${output[@]}; do
-		echo $i
-	done
+	echo $final
 }
+oper $1 $2
 exit 0
