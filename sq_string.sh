5ffc2f4f72159ebe5a9b348db5ac5850611c3c89
Steven Fairchild <steve51516@gmail.com>
2020-10-28 23:22:47 -0400

    getting closer

--- sq_string.sh
+++ sq_string.sh
@@ -1,26 +1,26 @@
-#!/bin/bash
-# https://www.codewars.com/kata/56dbe0e313c2f63be4000b25
-# Moves in squared strings Kata from codewars
-oper () {
-	local fct=$1
-	local s=$2
-	IFS=',' read -ra origArr <<< "$s"
-
-	if [ $fct == "vert_mirror" ]; then
-		for ((i=0; i < ${#origArr[@]}; i++)); do
-		sArr[$i]=$(echo ${sArr[$i]} | rev)
-	done
-	#elif [ $fct == "hor_mirror" ]; then
-		
-	fi
-
-	for item in ${sArr[@]}
-	do
-		printf "   %s\n" $item
-	done
+#!/bin/bash -x
+vertMirror() {
+	for ((i=0; i < ${#origArr[@]}; i++)); do
+                sArr[$i]=$(echo ${origArr[$i]} | rev)
+        done
+	final=$(echo ${sArr[*]} | tr ' ' '\r')
 }
-echo "original string: $2"
-#sArr=$(oper $1 $2)
-oper $1 $2
-printf "mirror string: "
+horMirror() {
+        local count=$((${#origArr[@]} - 1))
+        for ((i=$count,j=0; i >= 0; i--,j++)); do
+        	sArr[$i]=${origArr[$j]}
+        done
+	final=$(echo ${sArr[*]} | tr ' ' '\r')
+}
+oper() {
+	IFS=',' read -ra origArr <<< $2
+	if [ $1 == "vert_mirror" ]; then
+		local output=$(vertMirror)
+	elif [ $1 == "hor_mirror" ]; then
+		local output=$(horMirror)
+	fi
+	#echo ${output[*]}
+	echo $output
+}
+array=$(oper $1 $2)
 exit 0
