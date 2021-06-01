d9975678875f7dffa91fd6fc971bf9656332c164
steve51516 <steve51516@gmail.com>
2021-06-01 17:16:06 +0000

    rewrite to accept multiple databases as first arg

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -1,73 +1,91 @@
 #!/bin/bash
 # Create mysqldump backups with given arguements
-# Steven Fairchild 2021-04-20
-red="\e[1;31m"
-creset="\e[0m"
-if [[ "$(whoami)" != "root" ]]; then
-    echo -e "\n${red}This script must be ran as root!\nExiting...${creset}\n"
-    exit 0
-fi
-if [[ -z "$1" ]]; then
-    echo -e "database name must be passed as first aurguement\nExiting..."
-    exit 0
-elif [[ "$1" == "--all-databases" ]]; then
-    1="all_databases"
-fi
-
-cd /tmp # /tmp is a tmpfs filesystem on modern distrobutions, by default it is half the size of the total memory.
-        # if the backup is larger than half the memory size, it will fail before completing (or start swapping).
+# Steven Fairchild 2021-04-20 updated 2021-06-01
 
 # Set database and file variables
-DATABASE=$1
-BPATH=/data/backups/mariadb/$DATABASE
-FILE=$DATABASE-$(date +%F).dump
-if [[ ! -z "$2" ]]; then
-    echo "Using $2 as database user"
-    USER="$2"
+set_vars() {
+    export DATABASE=$1
+    export BPATH="/data/raid1/backups/mariadb/"
+    export USER="root"
+    export PASS="password"
+    export KEEP_DAYS="30"
+
+    export red="\e[1;31m"
+    export creset="\e[0m"
+    if [[ -z "$1" ]]; then
+        echo -e "database name must be passed as first aurguement\nExiting..."
+    exit 0
+    fi
+    if [[ ! -z "$2" ]]; then
+        echo "Using $2 as database user"
+        export USER="$2"
+    fi
+    if [[ ! -z "$3" ]]; then
+        echo "Using $3 as user password"
+        export PASS="$3"
+    fi
+}
+
+root_check() {
+    if [[ "$(whoami)" != "root" ]]; then
+        echo -e "\n${red}This script must be ran as root!\nExiting...${creset}\n"
+        exit 0
+    fi
+}
+
+usage() {
+    echo -e "Script usage\n\n\
+    \t--help or -h: print this message\n\n\
+    \tFirst arguement must be database list inside quotes even if only one database is specified\n\
+    \tOptional - second aurguement is the database username\n\
+    \tOptional - third aurguement is database password(will not be saved to bash history)\n\n\
+    \tExample 1: ./mysql_backup.sh \"database1 database2 database3\" database_username database_password\n\
+    \tExample 2: ./mysql_backup.sh \"database1\" database_username database_password\n\
+    \tNOTE:\n\
+    \t\tIf optional aurguments are not given the variables inside this script will be used\n\
+    \t\tEdit this script to change default settings\n"
+}
+
+main() {
+    cd /tmp # /tmp is a tmpfs filesystem on modern distrobutions, by default it is half the size of the total memory.
+        # if the backup is larger than half the memory size, it will fail before completing (or start swapping).
+    if [[ ! -d "$BPATH" ]]; then
+        mkdir -p "$BPATH"
+    fi
+    # create dump file
+    for db in $DATABASE; do
+        FILE="${db}-$(date +%F).dump"
+        mysqldump --opt --user=${USER} --password=${PASS} ${db} > "${FILE}"
+        echo "gzipping ${FILE}... this may take some time"
+        pigz -q ${FILE}
+        if [[ ${?} -eq 0 ]] && [[ -f "${FILE}.gz" ]]; then
+            echo "Successfully compressed ${FILE}.gz"
+            sha1sum "${FILE}.gz" > "${FILE}.gz.sha1"
+            # Move to permanent backup directory
+            mv ${FILE}.gz ${BPATH}
+            mv ${FILE}.gz.sha1 ${BPATH}
+            echo "Successfully created backup of ${db} located at ${BPATH}${FILE}.gz"
+            logger -t "mysql_backup.sh" "Successfully created backup of ${db} located at ${BPATH}${FILE}.gz"
+        else
+            echo "Failed to create ${FILE}.gz"
+        fi
+    done
+
+    echo -e "${red}Deleting files older than ${KEEP_DAYS} days in ${BPATH}${creset}"
+    find "${BPATH}" -mtime +"${KEEP_DAYS}" -print
+    echo -e "${red}Files shown will be deleted${creset}"
+    find "${BPATH}" -mtime +"${KEEP_DAYS}" -delete
+    logger -t "mysql_backup.sh" "Deleted files older than ${KEEP_DAYS} days in ${BPATH}"
+    cd - # Return to original directory
+    history -c # stop history from saving password if provided
+}
+
+if [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
+    usage
 else
-    USER="root"
-fi
-if [[ ! -z "$3" ]]; then
-    echo "Using $3 as user password"
-    PASS="$3"
-else
-    PASS="password"
-fi
-KEEP_DAYS="30"
-
-# Create backup folder if it doesn't exist
-if [[ ! -d "$BPATH" ]]; then
-    mkdir -p "$BPATH"
+    root_check
+    set_vars "$1"
+    main
 fi
 
-# create dump file
-if [[ "$DATABASE" == "all_databases" ]]; then
-    mysqldump --opt --user=${USER} --password=${PASS} --all-databases > ${FILE}
-else
-    mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
-fi
-
-# Gzip compress and create sha1 hash
-pigz -q $FILE
-sha1sum $FILE.gz > $FILE.gz.sha1
-
-# Delete backups if they already exist for today
-if [[ -f "$BPATH/$FILE.gz" ]] || [[ -f "$BPATH/$FILE.gz.sha1sum" ]]; then
-    rm "$BPATH/$FILE.gz"
-    rm "$BPATH/$FILE.gz.sha1"
-fi
-
-# Move to permanent backup directory
-mv $FILE.gz $BPATH
-mv $FILE.gz.sha1 $BPATH
-
-echo "Successfully created backup of $DATABASE located at $BPATH/$FILE.gz"
-logger "Successfully created backup of $DATABASE located at $BPATH/$FILE.gz"
-echo -e "${red}Deleting files older than $KEEP_DAYS days in $BPATH${creset}"
-find "$BPATH" -mtime +"$KEEP_DAYS" -print
-echo -e "${red}Files shown will be deleted${creset}"
-find "$BPATH" -mtime +"$KEEP_DAYS" -delete
-logger "Deleted files older than $KEEP_DAYS days in $BPATH"
-cd - # Return to original directory
-history -c # stop history from saving password if provided
 exit 0
