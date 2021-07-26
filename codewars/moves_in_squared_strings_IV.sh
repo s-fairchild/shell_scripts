67f2a837ce1df9df9ed3b56611ee27f5b29407b1
steve51516 <steve51516@gmail.com>
2021-07-26 17:52:10 +0000

    completed function diag_2_sym

--- codewars/moves_in_squared_strings_IV.sh
+++ codewars/moves_in_squared_strings_IV.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Steven Fairchild 2021-07-26
+
+# "abcd\nefgh\nijkl\nmnop" == "plhd\nokgc\nnjfb\nmiea"
 diag_2_sym() {
-    echo "Not Implimented Yet."
+    for (( i="${#arr[@]}"; i>=0; i-- )); do
+        for (( j=((${#arr[$i]} - 1)); j>=0; j-- )); do
+            sym["$j"]+="${arr[$i]:$j:1}"
+        done
+    done
+    k=0
+    for (( i=((${#arr[@]} - 1)); i>=0; i-- )); do
+        rev_sym["$i"]="${sym[$k]}"
+        ((k++))
+    done
+    echo "${rev_sym[@]}" | tr ' ' '\n'
 }
 rot_90_counter() {
     echo "Not Implimented Yet."
@@ -10,6 +22,7 @@ selfie_diag2_counterclock() {
     echo "Not Implimented Yet."
 }
 oper() {
+    export arr=( $(echo "$2" | sed 's/\\n/\n/g') )
     case "$1" in
     "diag_2_sym")
         diag_2_sym
@@ -20,7 +33,10 @@ oper() {
     "selfie_diag2_counterclock")
         selfie_diag2_counterclock
         ;;
+    *)
+        echo "Arguement error."
+        ;;
     esac
 }
 
-oper $1 $2
+oper "$1" "$2"
