e8e50e074626dee2a0192e8516d4bcf90bd84984
steve51516 <steve51516@gmail.com>
2021-07-26 13:45:23 +0000

    completed function diag_1_sym

--- codewars/moves_in_squared_strings_III.sh
+++ codewars/moves_in_squared_strings_III.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 # Steven Fairchild 2021-07-21
+# "abcd\nefgh\nijkl\nmnop" == "miea\nnjfb\nokgc\nplhd"
 rot_90_clock() {
-
+    echo "Not Implimented"
 }
-diag_1_sym() {
 
+# "abcd\nefgh\nijkl\nmnop" == "aeim\nbfjn\ncgko\ndhlp"
+diag_1_sym() {
+    for sq in "${arr[@]}"; do
+        for (( i=0; i<"${#arr[@]}"; i++ )); do
+            sym["$i"]+="${sq:$i:1}"
+        done
+    done
+    echo "${sym[@]}" | tr ' ' '\r'
 }
 selfie_and_diag1() {
-
+    echo "Not implimented"
 }
-# takes two parameters, 1: function, 2: string
 oper() {
+    export arr=( $(echo "$2" | sed 's/\\n/\n/g') )
     if [[ "$1" == "rot_90_clock" ]]; then
-        rot_90_clock $2
+        rot_90_clock "$arr"
     elif [[ "$1" == "diag_1_sym" ]]; then
-        diag_1_sym $2
+        diag_1_sym "$arr"
     elif [[ "$1" == "selfie_and_diag1" ]]; then
-        selfie_and_diag1 $2
+        selfie_and_diag1 "$arr"
     else
         echo "Arguement error"
     fi
